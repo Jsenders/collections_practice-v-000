30b4e5f9f6d607b8b45bf47d8714bb9a65b106a3
Joseph Senders <jasenders@gmail.com>
2018-08-09 20:56:52 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -5,3 +5,18 @@ end
 def sort_array_desc(integers)
   integers.sort.reverse
 end 
+
+def sort_array_char_count
+
+def swap_elements(array)
+  array.sort do |a, b|
+    if a = b 
+      0
+    elsif a < b
+      -1
+    elsif a > b 
+      1 
+    end
+  end
+    
+      
